18e0c34fb6b762e46aea142f4f89096d36281854
gar529 <russell.gregorya@gmail.com>
2022-07-02 09:50:58 -0400

    Added ReportingStructure type. Controller, Service, and Service Implementation for ReportingStructure type. Modified Employee type to add clone() method. Added test cases for retrieving ReportingStructure

---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,121 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureServiceImpl.class);
+    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public ReportingStructure read(String id) {
+        LOG.debug("Retrieving reporting structure with id [{}]", id);
+
+        Employee employee = employeeRepository.findByEmployeeId(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+        
+        DirectReportCounter counter = new DirectReportCounter(employee);
+        int numReports = counter.count();
+
+        return new ReportingStructure(employee, numReports);
+    }
+    
+    /**
+     * Class to count the number of distinct reports and build out the reporting structure of and employee
+     */
+    private class DirectReportCounter {
+		
+		// the number of distinct reports a single employee has
+		private int numReports;
+		
+		// the employee for the Reporting Structure
+		private Employee employee;
+		
+		// used to keep track of distinct reports
+		private Map<String, Employee> employeeMap; 
+		
+		private DirectReportCounter(final Employee employee) {
+			this.employee = employee;
+		}
+		
+		/**
+		 * Builds out the Reporting Structure and counts each of the employee's direct reports 
+		 * using the recursive traverse() method. Returns the total number of distinct reports
+		 * 
+		 * @return The total number of distinct reports
+		 */
+		private int count() {
+			
+			//reset the number of reports each time the method is called
+			numReports = 0; 
+			employeeMap = new HashMap<>();
+			
+			if (employee != null) {
+				employeeMap.put(employee.getEmployeeId(), employee);
+				
+				List<Employee> directReports = employee.getDirectReports();
+				
+				if (directReports != null) {
+					for (Employee report : directReports) {
+						
+						traverse(report);
+					}
+				}
+			}
+			return numReports;
+		}
+		
+		/**
+		 * Recursive method that traverses all children in a node and counts the total number of children
+		 * 
+		 * @param employee The node to be traversed
+		 */
+		private void traverse(Employee employee) {
+			
+			if (employee != null) {
+				
+				// only count and traverse unique records
+				if (!employeeMap.containsKey(employee.getEmployeeId())) {
+					// increment the numReports counter
+					numReports++;
+					
+					// get the built out employee object and apply it to the existing object
+					employee.clone(employeeRepository.findByEmployeeId(employee.getEmployeeId()));
+					
+					// add the unique record to the map
+					employeeMap.put(employee.getEmployeeId(), employee);
+					
+					List<Employee> directReports = employee.getDirectReports();
+					
+					if (directReports != null) {
+						for (Employee report : directReports) {
+							
+							traverse(report);
+						}
+					}
+				}
+				// if it's a duplicate, copy its data from the map
+				else if (employeeMap.containsKey(employee.getEmployeeId())) {
+					employee.clone(employeeMap.get(employee.getEmployeeId()));
+				}
+			}
+		}
+	}
+}
--- src/test/java/com/mindex/challenge/DataBootstrapTest.java
+++ src/test/java/com/mindex/challenge/DataBootstrapTest.java
@@ -2,6 +2,9 @@ package com.mindex.challenge;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.impl.ReportingStructureServiceImpl;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,9 @@ public class DataBootstrapTest {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    
+    @Autowired
+    private ReportingStructureServiceImpl reportingStructureServiceImpl;
 
     @Test
     public void test() {
@@ -26,5 +32,13 @@ public class DataBootstrapTest {
         assertEquals("Lennon", employee.getLastName());
         assertEquals("Development Manager", employee.getPosition());
         assertEquals("Engineering", employee.getDepartment());
+        
+        ReportingStructure reportingStructure1 = reportingStructureServiceImpl.read("62c1084e-6e34-4630-93fd-9153afb65309");
+        assertNotNull(reportingStructure1.getEmployee());
+        assertEquals(0, reportingStructure1.getNumberOfReports());
+        
+        ReportingStructure reportingStructure2 = reportingStructureServiceImpl.read("16a596ae-edd3-4847-99fe-c4518e82c86f");
+        assertNotNull(reportingStructure2.getEmployee());
+        assertEquals(4, reportingStructure2.getNumberOfReports());        
     }
 }
